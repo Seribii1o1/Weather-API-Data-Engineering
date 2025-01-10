56e1a71769ebaea8aad2e2c14565238ad99884d6
Seribii1o1 <164199835+Seribii1o1@users.noreply.github.com>
2025-01-09 19:43:08 -0500

    commit 8

--- weather_api/notebooks/data_transform.py
+++ weather_api/notebooks/data_transform.py
@@ -4,9 +4,9 @@ df = pd.read_csv('/Users/sa26/Documents/GitHub/Weather-API-Data-Engineering/weat
 df['time'] = pd.to_datetime(df['time'])
 df['month'] = df['time'].dt.month
 df['year'] = df['time'].dt.year
-monthly_medians = df.groupby('month')[['temperature_2m', 'relative_humidity_2m', 'precipitation', 'surface_pressure']].median()
+monthly_medians_by_year = df.groupby(['year', 'month'])[['temperature_2m', 'relative_humidity_2m', 'precipitation', 'surface_pressure']].median()
 yearly_medians = df.groupby('year')[['temperature_2m', 'relative_humidity_2m', 'precipitation', 'surface_pressure']].median()
-print(monthly_medians)
+print(monthly_medians_by_year)
 print(yearly_medians)
 harvest_df = pd.read_csv('/Users/sa26/Documents/GitHub/Weather-API-Data-Engineering/weather_api/data/csv/br_final.csv').dropna() 
 minas_harvest = harvest_df[harvest_df['subdivision'] == 'Minas Gerais']
@@ -16,7 +16,7 @@ merged_data = pd.merge(yearly_medians, yearly_medians_harvest, on='year')
 # Display the merged dataframe
 print(merged_data)
 file_path1 = "/Users/sa26/Documents/GitHub/Weather-API-Data-Engineering/weather_api/data/csv/monthly_medians.csv"
-monthly_medians.to_csv(file_path1, index=False)
+monthly_medians_by_year.to_csv(file_path1, index=False)
 file_path2 = "/Users/sa26/Documents/GitHub/Weather-API-Data-Engineering/weather_api/data/csv/yearly_medians.csv"
 yearly_medians.to_csv(file_path2, index=False)
 file_path3 = "/Users/sa26/Documents/GitHub/Weather-API-Data-Engineering/weather_api/data/csv/merged_data.csv"
